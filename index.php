54913b352800c348775498189f88efcf1a374dd9
lintankwgbn <lintankwgbn@gmail.com>
2025-11-14 04:07:35 +0700

    update index page

--- index.php
+++ index.php
@@ -5,6 +5,6 @@ require __DIR__ . '/vendor/autoload.php';
 echo view('index', [
     'title' => 'Blade Index Page',
     'greeting' => 'This is a Blade Index Page',
-    'description' => 'An index page is the main entry point to a website, often called a homepage or landing page, that serves as the primary gateway for visitors. <br>It provides an overview of the site\'s content and includes navigation to guide users to other pages. <br>On the web, the index page is also the default page that loads when someone visits a website without specifying a particular page, <br>with path <b>/app/views/index.blade.php</b> being the most common default name for this file.',
+    'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 ]);
 
